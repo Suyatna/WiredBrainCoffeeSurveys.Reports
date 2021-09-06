998cd48b5b24597673072ae906e735fcba65ad29
Suyatna <zuyatna@gmail.com>
2021-09-06 20:16:32 +0700

    25 - Demo - Organizing the Application Reports

--- WiredBrainCoffeeSurveys.Reports/Program.cs
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace WiredBrainCoffeeSurveys.Reports
 {
@@ -7,14 +8,67 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
+            GenerateWinnerEmails();
+
             GenerateTasksReport();
+
+            GenerateCommentsReport();
+        }
+
+        public static void GenerateWinnerEmails()
+        {
+            var selectedEmails = new List<string>();
+            int counter = 0;
+
+            Console.WriteLine(Environment.NewLine + "Selected Winners Output:");
+            while (selectedEmails.Count < 2 && counter < Q1Results.Responses.Count)
+            {
+                var currentItem = Q1Results.Responses[counter];
+
+                if (currentItem.FavoriteProduct == Q1Results.FavoriteProduct)
+                {
+                    selectedEmails.Add(currentItem.EmailAddress);
+                    Console.WriteLine(currentItem.EmailAddress);
+                }
+
+                counter++;
+            }
+
+            File.WriteAllLines("WinnersReport.csv", selectedEmails);
+        }
+
+        public static void GenerateCommentsReport()
+        {
+            var comments = new List<string>();
+
+            Console.WriteLine(Environment.NewLine + "Comments Output:");
+            for (var i = 0; i < Q1Results.Responses.Count; i++)
+            {
+                var currentResponse = Q1Results.Responses[i];
+
+                if (currentResponse.WouldRecommend < 7.0)
+                {
+                    Console.WriteLine(currentResponse.Comments);
+                    comments.Add(currentResponse.Comments);
+                }
+            }
+
+            foreach (var response in Q1Results.Responses)
+            {
+                if (response.AreaToImprove == Q1Results.AreaToImprove)
+                {
+                    Console.WriteLine(response.Comments);
+                    comments.Add(response.Comments);
+                }
+            }
+
+            File.WriteAllLines("CommentsReport.csv", comments);
         }
 
         public static void GenerateTasksReport()
         {
-            var tasks = new List<String>();
+            var tasks = new List<string>();
 
-            // Calculate Values
             double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
             double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
 
@@ -29,7 +83,7 @@ namespace WiredBrainCoffeeSurveys.Reports
             }
             else
             {
-                tasks.Add("Work with employee for improvement ideas");
+                tasks.Add("Work with employees for improvement ideas.");
             }
 
             if (responseRate < .33)
@@ -44,6 +98,30 @@ namespace WiredBrainCoffeeSurveys.Reports
             {
                 tasks.Add("Rewards participants with discount coffee coupon.");
             }
+
+            switch (Q1Results.AreaToImprove)
+            {
+                case "RewardsProgram":
+                    tasks.Add("Revisit the rewards deals.");
+                    break;
+                case "Cleanliness":
+                    tasks.Add("Contact the cleaning vendor.");
+                    break;
+                case "MobileApp":
+                    tasks.Add("Contact the consulting firm about app.");
+                    break;
+                default:
+                    tasks.Add("Investigate individual comments for ideas.");
+                    break;
+            }
+
+            Console.WriteLine(Environment.NewLine + "Tasks Output:");
+            foreach (var task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+
+            File.WriteAllLines("TasksReport.csv", tasks);
         }
     }
 }
